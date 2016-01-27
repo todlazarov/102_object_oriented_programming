d5bdf771634d1f714691719c86d020bd3c0a0f15
Tod Lazarov <todlazarov1@gmail.com>
2016-01-27 16:37:26 -0500

    Add the ability to input your name

--- RPS_OOP.rb
+++ RPS_OOP.rb
@@ -1,9 +1,10 @@
 class Player
-  attr_accessor :move
+  attr_accessor :move, :name
 
   def initialize(player_type = :human)
     @player_type = player_type
     @move = nil
+    set_name
   end
 
   def human?
@@ -22,6 +23,21 @@ class Player
       self.move = mv
     else
       self.move = ['rock', 'paper', 'scissors'].sample
+    end
+  end
+
+  def set_name
+    n = ''
+    if human?
+      loop do
+        puts "What is your name?"
+        n = gets.chomp
+        break unless n.empty?
+        puts "You have to put in a value."
+      end
+      self.name = n
+    else
+      self.name = ['R2D2', 'Chappie', 'Sonny', 'Optimus Prime', 'Well-E'].sample
     end
   end
 end
@@ -43,20 +59,20 @@ class RPSGame
   end
 
   def display_winner
-    puts "You chose: #{human.move}"
-    puts "The computer chose: #{computer.move}"
+    puts "#{human.name} chose: #{human.move}"
+    puts "#{computer.name} chose: #{computer.move}"
 
     if human.move == "rock"
-      puts "You win!" if computer.move == "scissors"
-      puts "Computer wins!" if computer.move == "paper"
+      puts "#{human.name} win!" if computer.move == "scissors"
+      puts "#{computer.name} wins!" if computer.move == "paper"
       puts "Its a tie!" if computer.move == "rock"
     elsif human.move == "paper"
-      puts "You win!" if computer.move == "rock"
-      puts "Computer wins!" if computer.move == "scissors"
+      puts "#{human.name} win!" if computer.move == "rock"
+      puts "#{computer.name} wins!" if computer.move == "scissors"
       puts "Its a tie!" if computer.move == "paper"
     elsif human.move == "scissors"
-      puts "You win!" if computer.move == "paper"
-      puts "Computer wins!" if computer.move == "rock"
+      puts "#{human.name} win!" if computer.move == "paper"
+      puts "#{computer.name} wins!" if computer.move == "rock"
       puts "Its a tie!" if computer.move == "scissors"
     end
   end
