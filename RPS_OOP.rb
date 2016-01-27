0d9b30017938dc99370a4498b88ecb6e2f1b7c73
Tod Lazarov <todlazarov1@gmail.com>
2016-01-27 17:49:34 -0500

    Rubocop suggestions. Improved overall logic and fix small offenses.

--- RPS_OOP.rb
+++ RPS_OOP.rb
@@ -18,29 +18,15 @@ class Move
   end
 
   def >(other_move)
-    if rock?
-      return true if other_move.scissors?
-      return false
-    elsif paper?
-      return true if other_move.rock?
-      return false
-    elsif scissors?
-      return true if other_move.paper?
-      return false
-    end
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
   end
 
   def <(other_move)
-    if rock?
-      return true if other_move.paper?
-      return false
-    elsif paper?
-      return true if other_move.scissors?
-      return false
-    elsif scissors?
-      return true if other_move.rock?
-      return false
-    end
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
   end
 
   def to_s
@@ -106,10 +92,12 @@ class RPSGame
     puts "Thank you for playing Rock, Paper, Scissors. Good bye!"
   end
 
-  def display_winner
+  def display_selections
     puts "#{human.name} chose: #{human.move}"
     puts "#{computer.name} chose: #{computer.move}"
+  end
 
+  def display_winner
     if human.move > computer.move
       puts "#{human.name} won!"
     elsif human.move < computer.move
@@ -124,12 +112,11 @@ class RPSGame
     loop do
       puts "Would you like to play again?(y/n)"
       answer = gets.chomp
-      break if ['y','n'].include? answer
+      break if ['y', 'n'].include? answer
       puts "Sorry, you must pick y or n."
     end
 
     return true if answer == 'y'
-    return false
   end
 
   def play
@@ -137,6 +124,7 @@ class RPSGame
     loop do
       human.choose
       computer.choose
+      display_selections
       display_winner
       break unless play_again?
     end
