407cb870d0ecb03fb48ed3c4915b9323a130aa5b
Tod Lazarov <todlazarov1@gmail.com>
2016-01-27 16:49:43 -0500

    Refactor to have two subclasses, Human and Computer.

--- RPS_OOP.rb
+++ RPS_OOP.rb
@@ -1,44 +1,42 @@
 class Player
   attr_accessor :move, :name
 
-  def initialize(player_type = :human)
-    @player_type = player_type
-    @move = nil
+  def initialize
     set_name
   end
+end
 
-  def human?
-    @player_type == :human
-  end
-
+class Human < Player
   def choose
-    if human?
-      mv = ''
-      loop do
-        puts "Choose your option:(rock, paper or scissors)"
-        mv = gets.chomp
-        break if ['rock', 'paper', 'scissors'].include? mv.downcase
-        puts "Invalid input, please choose again."
-      end
-      self.move = mv
-    else
-      self.move = ['rock', 'paper', 'scissors'].sample
+    mv = ''
+    loop do
+      puts "Choose your option:(rock, paper or scissors)"
+      mv = gets.chomp
+      break if ['rock', 'paper', 'scissors'].include? mv.downcase
+      puts "Invalid input, please choose again."
     end
+    self.move = mv
   end
 
   def set_name
     n = ''
-    if human?
-      loop do
-        puts "What is your name?"
-        n = gets.chomp
-        break unless n.empty?
-        puts "You have to put in a value."
-      end
-      self.name = n
-    else
-      self.name = ['R2D2', 'Chappie', 'Sonny', 'Optimus Prime', 'Well-E'].sample
+    loop do
+      puts "What is your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "You have to put in a value."
     end
+    self.name = n
+  end
+end
+
+class Computer < Player
+  def choose
+    self.move = ['rock', 'paper', 'scissors'].sample
+  end
+
+  def set_name
+    self.name = ['R2D2', 'Chappie', 'Sonny', 'Optimus Prime', 'Well-E'].sample
   end
 end
 
@@ -46,8 +44,8 @@ class RPSGame
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new(:computer)
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def display_welcome_message
